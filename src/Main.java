b1379d1c8bb83dcdbeb7ec16aefd9b74b1671563
Svetlana Domakhina <domakhina.s@gmail.com>
2023-11-23 11:48:53 -0800

    Task1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ваши бонусные мили");
+
+        int ticket = 13_676;
+        int mile = 20;
+        int bonusMiles = ticket / mile;
+        System.out.println(bonusMiles);
+
+    }
+}
